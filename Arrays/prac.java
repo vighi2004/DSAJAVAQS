cb47761553dd7dde378921c8779866513ed0287d
vighi2004 <vighneshtakke4@gmail.com>
2025-02-15 20:37:42 +0530

    add files

---
+++ Arrays/prac.java
@@ -0,0 +1,56 @@
+public class prac{
+    public static int count=1;
+    public static void mergesort(int[] arr,int si ,int end) {
+        if(si>=end){
+            return;
+        }
+        int mid=si+(end-si)/2;
+        //left array
+        mergesort(arr,si,mid);
+        //right array
+        mergesort(arr, mid+1, end);
+        merge(arr,si,mid,end);
+    }
+    public static void merge(int[] arr,int si,int mid,int end) {
+        int i=si;
+        int j=mid+1;
+        int k=0;
+        int temp[]=new int[end-si+1];
+        //sorting full array and storing in temp array.
+        while(i<=mid&&j<=end){
+            if(arr[i]>arr[j]){
+                count+=1;
+                temp[k]=arr[j];
+                j++;
+            }
+            else{
+                temp[k]=arr[i];
+                i++;
+            }
+            k++;
+        }
+        //left array if remain than store
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        //right array if remain than store.
+        while(j<=end){
+            temp[k++]=arr[j++];
+        }
+        //copying array
+        for(k=0,i=si;k<temp.length;k++,i++){
+            arr[i]=temp[k];
+        }
+
+    }
+   public static void main(String[] args) {
+    int arr[]={2,4,1,3,5};
+    mergesort(arr,0,arr.length-1);
+    if(count>0){
+    System.out.println("inversion required "+count); 
+    }
+    else{
+        System.out.println("Array is already sorted");
+    }
+}
+}
---
+++ Arrays/sortString.java
@@ -0,0 +1,54 @@
+//sorting string using merge sort.
+public class sortString {
+    public static void mergesort(String[] arr,int si ,int end) {
+        if(si>=end){
+            return;
+        }
+        int mid=si+(end-si)/2;
+        //left array
+        mergesort(arr,si,mid);
+        //right array
+        mergesort(arr, mid+1, end);
+        merge(arr,si,mid,end);
+    }
+    public static void merge(String[] arr,int si,int mid,int end) {
+        int i=si;
+        int j=mid+1;
+        int k=0;
+        String temp[]=new String[end-si+1];
+        //sorting full array and storing in temp array.
+        while(i<=mid&&j<=end){
+            if((arr[i].compareTo(arr[j])<0)){
+                temp[k]=arr[i];
+                i++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        //left array if remain than store
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        //right array if remain than store.
+        while(j<=end){
+            temp[k++]=arr[j++];
+        }
+        //copying array
+        for(k=0,i=si;k<temp.length;k++,i++){
+            arr[i]=temp[k];
+        }
+
+        
+    }
+   public static void main(String[] args) {
+    String arr[]={"sun","earth","mercury","mars"};
+    mergesort(arr,0,arr.length-1);
+    for(int p=0;p<arr.length;p++){
+        System.out.print(" "+arr[p]);
+    }
+}    
+}
+
