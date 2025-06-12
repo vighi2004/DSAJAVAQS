fdbdee55652ed5992a42bb96bb54cc55cfdfba3c
vighi2004 <vighneshtakke4@gmail.com>
2025-06-12 21:39:25 +0530

    add files

--- BST/LargestSizeOfBST.java
+++ BST/LargestSizeOfBST.java
@@ -18,35 +18,41 @@ public class LargestSizeOfBST {
         int size;
         int min;
         int max;
+        int sum;
 
-        public Info(boolean isBST,int size,int min,int max){
+        public Info(boolean isBST,int size,int min,int max,int sum){
             this.isBST=isBST;
             this.size=size;
             this.min=min;
             this.max=max;
+            this.sum=sum;
         }
     }
+    static int Usum=0;
     public static Info LargestBST(node root){
         if(root==null){
-            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE,0);
         }
         Info leftinfo=LargestBST(root.left);
         Info rightInfo=LargestBST(root.right);
         //size
         int size=leftinfo.size+rightInfo.size+1;
+        //sum
+        int sum=leftinfo.sum+rightInfo.sum+root.data;
         //min
         int min=Math.min(root.data,Math.min(leftinfo.min,rightInfo.min));
         //max
         int max=Math.max(root.data,Math.max(leftinfo.max,rightInfo.max));
         //isBST valid logic
         if(root.data<=leftinfo.max||root.data>=rightInfo.min){
-            return new Info(false,size,min,max);
+            return new Info(false,size,min,max,sum);
         }
         if(leftinfo.isBST&&rightInfo.isBST){
+            Usum=Math.max(Usum,sum);
             maxSize=Math.max(maxSize,size);
-            return new Info(true, size, min, max);
+            return new Info(true, size, min, max,sum);
         }
-        return new Info(false, size, min, max);
+        return new Info(false, size, min, max,sum);
 
     }
     public static void main(String[] args) {
@@ -68,6 +74,7 @@ public class LargestSizeOfBST {
                 root.right.right.right=new node(80);
                 LargestBST(root);
                 System.out.println("Max size of BST is: "+maxSize);
+                System.out.println(Usum);
                 
 
      }
