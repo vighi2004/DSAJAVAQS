73bcab35c0d3053e4e754006f4ab7216c7c8f14c
vighi2004 <vighneshtakke4@gmail.com>
2025-06-14 23:25:05 +0530

    add files

---
+++ Heaps/connectNropes.java
@@ -0,0 +1,19 @@
+package Heaps;
+import java.util.*;
+public class connectNropes {
+    public static void main(String[] args) {
+        int arr[]={4,3,2,6};
+        PriorityQueue <Integer> PQ=new PriorityQueue<>();
+        for(int i=0;i<arr.length;i++){
+            PQ.add(arr[i]);
+        }
+        while(!PQ.isEmpty()&&PQ.size()!=1){
+               int a=PQ.remove();
+               int b=PQ.remove();
+               PQ.add(a+b);
+        }
+        System.out.println("Minimum cost to connect ropes: "+PQ.peek());
+
+    }
+    
+}
