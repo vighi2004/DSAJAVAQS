9bfb770f7c2ea8dabd2229c13a8cf751e45df16a
vighi2004 <vighneshtakke4@gmail.com>
2025-06-16 13:46:06 +0530

    add files

---
+++ Heaps/HalveArraySum.java
@@ -0,0 +1,28 @@
+//Minimum operation to halve Array sum;
+package Heaps;
+import java.util.*;
+public class HalveArraySum {
+    public static void main(String[] args) {
+        PriorityQueue <Double> PQ=new PriorityQueue<>(Comparator.reverseOrder());
+        int op=0;
+        double arr[]={1,5,8,19};
+        for(int i=0;i<arr.length;i++){
+            PQ.add(arr[i]);
+        }
+        double Totalsum=0;
+        for(int i=0;i<arr.length;i++){
+           Totalsum=Totalsum+arr[i];
+        }
+        double currsum=Totalsum;
+        double TargetSum=Totalsum/2;
+        while(currsum>TargetSum){
+             double num=PQ.remove();
+             double val=num/2;
+             PQ.add(val);
+             currsum=currsum-val;
+             System.out.println(currsum);
+             op++;
+        }
+        System.out.println("Operation required: "+op);
+    }
+}
