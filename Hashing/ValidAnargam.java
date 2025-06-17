990733852f3b2d26c30db116f37b2411530c8d6d
vighi2004 <vighneshtakke4@gmail.com>
2025-06-17 21:07:59 +0530

    add files

--- Hashing/ValidAnargam.java
+++ Hashing/ValidAnargam.java
@@ -3,19 +3,33 @@ import java.util.*;
 public class ValidAnargam {
     public static boolean check(String r,HashMap <Character,Integer>hm){
         for(int i=0;i<r.length();i++){
-            if(!hm.containsKey(r.charAt(i))){
-                return false;
+            char ch=r.charAt(i);
+            if(hm.get(ch)!=null){
+            if(hm.get(ch)==1){
+                hm.remove(ch);
+            }
+            else {
+                hm.put(ch,hm.get(ch)-1);
+             }
+            }
+           else{
+              return false;
             }
         }
+       if(hm.isEmpty()){
         return true;
+       }
+       else{
+          return false;
+       }
 
-    }
+}
     public static void main(String[] args) {
         HashMap <Character,Integer> hm=new HashMap<>();
         String s="race";
         String r="care";
         for(int i=0;i<s.length();i++){
-            hm.put(s.charAt(i), 1);
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i),0)+1);
         }
         if(check(r,hm)){
              System.out.println("TRUE");
