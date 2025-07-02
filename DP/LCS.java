31b85703023fbdabd0357c109a56088a4ddad8c4
vighi2004 <vighneshtakke4@gmail.com>
2025-07-02 22:58:03 +0530

    add files

---
+++ DP/LCS.java
@@ -0,0 +1,73 @@
+//finding longest common subsequence.
+package DP;
+public class LCS {
+    //using recursion O(2^mn)
+    public static int LCS(String str1,String str2,int n,int m){
+        if(n==0||m==0){
+            return 0;
+        }
+        if(str1.charAt(n-1)==str2.charAt(m-1)){
+           return 1+LCS(str1, str2, n-1, m-1);
+        }
+        else{
+            int ans1=LCS(str1, str2, n-1, m);
+            int ans2=LCS(str1, str2, n, m-1);
+            return Math.max(ans1,ans2);
+        }
+    }
+    //using DP memiozation approach O(N*M).
+     public static int LCSM(String str1,String str2,int n,int m,int dp[][]){
+        if(n==0||m==0){
+            return 0;
+        }
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+        if(str1.charAt(n-1)==str2.charAt(m-1)){
+           return dp[n][m]=1+LCSM(str1, str2, n-1, m-1,dp);
+        }
+        else{
+            int ans1=LCSM(str1, str2, n-1, m,dp);
+            int ans2=LCSM(str1, str2, n, m-1,dp);
+            return dp[n][m]=Math.max(ans1,ans2);
+        }
+    }
+    //using DP memiozation approach O(N*M).
+     public static void LCST(String str1,String str2,int n,int m,int dp[][]){
+        for(int i=1;i<str1.length()+1;i++){
+            for(int j=1;j<str2.length()+1;j++){ 
+               if(str1.charAt(n-1)==str2.charAt(m-1)){
+                   dp[i][j]=dp[i-1][j-1]+1;//diagonal uppere check
+                 }
+                 //different then compare up and left which is max.
+               else{
+                  int ans1=dp[i-1][j];
+                  int ans2=dp[i][j-1];
+                  dp[i][j]=Math.max(ans1,ans2);
+        }}
+    }
+    System.out.println("LCS(DPT): "+dp[str1.length()][str2.length()]);
+    }
+    public static void main(String[] args) {
+        String str1="abcde";
+        String str2="ace";
+        System.out.println("LCS is: "+LCS(str1,str2,str1.length(),str2.length()));
+        System.out.println("using DP memiozation");
+        int dp[][]=new int[str1.length()+1][str2.length()+1];
+        for(int i=0;i<str1.length()+1;i++){
+            for(int j=0;j<str2.length()+1;j++){
+                dp[i][j]=-1;
+            }
+        }
+        System.out.println("LCS(DPM): "+LCSM(str1, str2, str1.length(), str2.length(),dp));
+        System.out.println("using tabulation:-");
+        int[][] dp1=new int[str1.length()+1][str2.length()+1];
+          for(int i=0;i<str1.length()+1;i++){
+                dp[i][0]=0;
+            }
+          for(int i=0;i<str2.length()+1;i++){
+                dp[0][i]=0;
+          }
+        LCST(str1, str2, str1.length(), str2.length(), dp1);
+    }
+}
