63b61dfd6d06a97643e2d1bbbed7533142dd1082
vighi2004 <vighneshtakke4@gmail.com>
2025-07-06 13:22:29 +0530

    add files

--- DP/MCM.java
+++ DP/MCM.java
@@ -20,7 +20,7 @@ public class MCM {
 
     }
      //using memoization
-     public static int mcmT(int arr[],int i,int j,int dp[][]){
+     public static int mcmM(int arr[],int i,int j,int dp[][]){
         if(i==j){
             return 0;
         }
@@ -30,13 +30,33 @@ public class MCM {
         int ans=Integer.MAX_VALUE;
         //spliting
         for(int k=i;k<j;k++){
-            int cost1=mcm(arr, i, k);
-            int cost2=mcm(arr, k+1, j);
+            int cost1=mcmM(arr, i, k,dp);
+            int cost2=mcmM(arr, k+1, j,dp);
             int cost3=arr[i-1]*arr[k]*arr[j];
             int finalcost=cost1+cost2+cost3;
             ans= Math.min(ans,finalcost);
         }
         return dp[i][j]=ans;
+
+    }
+    //using Tabulation
+    public static void mcmT(int arr[],int n){
+        int dp[][]=new int[n][n];
+        //by default all are zero so diagonally will be zero.
+        for(int len=2;len<n;len++){
+            for(int i=1;i<=n-len;i++){//row
+                int j=i+len-1;//column
+                dp[i][j]=Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int cost1=dp[i][k];
+                    int cost2=dp[k+1][j];
+                    int cost3=arr[i-1]*arr[k]*arr[j];
+                    int mincost=cost1+cost2+cost3;
+                    dp[i][j]=Math.min(dp[i][j], mincost);
+                }
+            }
+        }
+        System.out.println("minimum cost is: "+dp[1][n-1]);
 
     }
     public static void main(String[] args) {
@@ -48,7 +68,9 @@ public class MCM {
         for(int i=0;i<n;i++){
            Arrays.fill(dp[i], -1);//rowise all -1;
         }
-        System.out.println("mcmT: "+mcmT(arr, 1, n-1,dp));
+        System.out.println("mcmM: "+mcmM(arr, 1, n-1,dp));
+        System.out.println("using Tabulation:-");
+        mcmT(arr,n);
 }
     
 }
