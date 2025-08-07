8fa14db8644d297d227b1300f2bbd884cdbde362
vighi2004 <162781465+vighi2004@users.noreply.github.com>
2025-08-07 10:03:37 +0530

    Update CountDistictSubArr.java

--- Hashing/CountDistictSubArr.java
+++ Hashing/CountDistictSubArr.java
@@ -1,20 +1,30 @@
-//counting subarrays which are distinct.
+//counting size of  subarrays which are distinct .
 package Hashing;
 import java.util.*;
 public class CountDistictSubArr {
     public static void main(String[] args) {
-    int arr[]={1, 2, 3};
-    HashSet <String> set=new HashSet<>();
-    for(int i=0;i<arr.length;i++){
-       int j=i;
-       String str="";
-       while(j<arr.length){
-            str += Integer.toString(arr[j]);
-            set.add(str);
-            j++;
-       }
-    }
-
-    System.out.println("count fo distict subarrays: "+set.size());
+   	HashSet <String>  set=new HashSet<>();
+		int arr[]={1,2,10};
+		for(int i=0;i<arr.length;i++){
+		    int j=i;
+		    StringBuilder res=new StringBuilder(" ");
+		   while(j<arr.length){
+		       String str = String.valueOf(arr[j]);
+		       str+=',';
+		       res=res.append(str);
+		       set.add(res.toString());
+		       j++;
+		   }
+		   
+		}
+		int count=0;
+		for(String k:set){
+		   for(int i=0;i<k.length();i++){
+		       if(k.charAt(i)==','){
+		           count+=1;
+		       }
+		   }
+		}
+		System.out.println(count);
 }
 }
