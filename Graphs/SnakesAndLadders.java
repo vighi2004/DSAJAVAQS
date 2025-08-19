57301c149ae1354bb020225697afed75db06b2fa
vighi2004 <vighneshtakke4@gmail.com>
2025-08-19 12:17:15 +0530

    add files

--- Graphs/SnakesAndLadders.java
+++ Graphs/SnakesAndLadders.java
@@ -1,4 +1,4 @@
-//snakes and ladders game .
+//snakes and ladders game.
 package Graphs;
 import java.util.*;
 public class SnakesAndLadders {
@@ -11,33 +11,60 @@ public class SnakesAndLadders {
         }
     }
     public static int getMinDiceThrows(int board[][]){
-        int n=board.length;
-        boolean visit[][]=new boolean[n][n];
-        Queue<pair> q=new LinkedList<>();
-        q.add(new pair(board[n-1][0],0));
-        visit[n-1][0]=true;
-        while(!q.isEmpty()){
-            pair curr=q.poll();
-            int position=curr.position;
-            if(position==n*n){
-                return curr.step;
+       int n=board.length;
+       int L2R=1;
+       int game[]=new int[n*n];
+       boolean visited[]=new boolean[n*n];
+       Arrays.fill(game,-1);
+       int index=0;
+       //lets connvert the 2d array to 1d array.
+       for(int row=n-1;row>=0;row--){
+          if(L2R%2!=0){
+             //left to right
+             for(int col=0;col<n;col++){
+                if(board[row][col]!=-1){
+                    game[index]=board[row][col];
+                }
+                index++;
+             }
+          }
+          else{
+            //right to left
+             for(int col=n-1;col>=0;col--){
+                if(board[row][col]!=-1){
+                    game[index]=board[row][col];
+                }
+                index++;
+             }
             }
-            for(int i=1;i<6;i++){
-                int nextpos=position+i;
-                if(board[(nextpos)/n][(nextpos)%n]!=-1){
-                     nextpos=board[(nextpos)/n][(nextpos)%n];
-                    q.add(new pair(nextpos, curr.step + 1));
-                    visit[(nextpos)/n][(nextpos)%n]=true;
-                }
-                else{
-                    if(nextpos<=n*n && !visit[(nextpos)/n][(nextpos)%n]){
-                        q.add(new pair(nextpos, curr.step + 1));
-                        visit[(nextpos)/n][(nextpos)%n]=true;
-                    }
-                }
+            L2R++;
+       }
+       //lets play game and check the minimum number of steps required to reach the end.
+       Queue<pair> q=new LinkedList<>();
+       q.add(new pair(0, 0));
+       visited[0]=true;
+       while(!q.isEmpty()){
+        pair curr=q.poll();
+        int pos=curr.position;
+        if(pos==n*n-1){
+          return curr.step;
+        }
+        for(int dice=1;dice<7;dice++){
+            int nextpos=pos+dice;
+            if(nextpos>=n*n){
+                break;
+            }
+            if(game[nextpos]!=-1){
+                nextpos=game[nextpos];
+            }
+            if(!visited[nextpos]){
+                visited[nextpos]=true;
+                q.add(new pair(nextpos, curr.step + 1));
             }
         }
-        return -1;
+       }
+       return -1;
+               
     }
     public static void main(String[] args) {
         int  board[][] = {{-1,-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1,-1},{-1,35,-1,-1,13,-1},{-1,-1,-1,-1,-1,-1},{-1,15,-1,-1,-1,-1}};
