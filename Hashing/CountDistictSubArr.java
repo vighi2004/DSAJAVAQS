1fa6afa4a65083b0896766188aebecb56b217513
vighi2004 <vighneshtakke4@gmail.com>
2025-08-21 20:08:21 +0530

    add files

--- Hashing/CountDistictSubArr.java
+++ Hashing/CountDistictSubArr.java
@@ -5,7 +5,7 @@ public class CountDistictSubArr {
     public static void main(String[] args) {
    	HashSet <List<Integer>>  set=new HashSet<>();
 		int arr[]={1,2,10};
-	        int size=0;
+	        int totalSize=0;
 		for(int i=0;i<arr.length;i++){
 		    int j=i;
 		    List<Integer> subarray = new ArrayList<>();
